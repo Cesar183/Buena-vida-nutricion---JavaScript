1e97055a42c72a76c4cfe6906ecf096a6239cb87
Cesar <cesar_a_mar_@hotmail.com>
2022-07-20 20:25:22 -0500

    Masa corporal, primer paciente

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,11 @@
-var titulo =  document.querySelector(".titulo");
-console.log(titulo);
-console.log(titulo.textContent);
-titulo.textContent="Buena Vida Nutrición";
+var paciente = document.querySelector("#primer-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = parseInt(tdPeso.textContent);
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = parseInt(tdAltura.textContent);
+
+var tdIMC = paciente.querySelector(".info-imc");
+var imc = peso / (altura * altura);
+tdIMC.textContent = imc;
