e53afe5b33eafcff19b8c9ef7c16d68ddae6ce81
Cesar <cesar_a_mar_@hotmail.com>
2022-07-21 21:50:25 -0500

    condicionales

--- js/principal.js
+++ js/principal.js
@@ -9,3 +9,22 @@ var altura = parseInt(tdAltura.textContent);
 var tdIMC = paciente.querySelector(".info-imc");
 var imc = peso / (altura * altura);
 tdIMC.textContent = imc;
+
+pesoEsValido = true;
+alturaEsValida = true;
+
+if(peso < 0 || peso > 1000)
+{
+    tdIMC.textContent = ("Peso incorrecto");
+    pesoEsValido = false;
+}
+if(altura < 0 || altura > 4)
+{
+    tdIMC.textContent = ("Altura incorrecta");
+    alturaEsValida = false;
+}
+if(pesoEsValido && alturaEsValida)
+{
+    var imc = peso / (altura * altura);
+    tdIMC.textContent = imc;
+}
