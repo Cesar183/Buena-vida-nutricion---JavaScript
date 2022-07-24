d8b0ec576fe32458f42429395a6dbdef1864c239
Cesar <cesar_a_mar_@hotmail.com>
2022-07-23 21:46:18 -0500

    completando el IMC y estilo a las celdas

--- js/principal.js
+++ js/principal.js
@@ -1,30 +1,42 @@
-var paciente = document.querySelector("#primer-paciente");
-
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = parseInt(tdPeso.textContent);
-
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = parseInt(tdAltura.textContent);
-
-var tdIMC = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura);
-tdIMC.textContent = imc;
-
-pesoEsValido = true;
-alturaEsValida = true;
-
-if(peso < 0 || peso > 1000)
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
+for(var i=0; i<pacientes.length; i++)
 {
-    tdIMC.textContent = ("Peso incorrecto");
-    pesoEsValido = false;
-}
-if(altura < 0 || altura > 4)
-{
-    tdIMC.textContent = ("Altura incorrecta");
-    alturaEsValida = false;
-}
-if(pesoEsValido && alturaEsValida)
-{
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = parseFloat(tdPeso.textContent);
+    var tdNombre = paciente.querySelector(".info-nombre");
+    var nombre = tdNombre.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = parseFloat(tdAltura.textContent);
+
+    var tdIMC = paciente.querySelector(".info-imc");
     var imc = peso / (altura * altura);
     tdIMC.textContent = imc;
+
+    pesoEsValido = true;
+    alturaEsValida = true;
+
+    if(peso < 0 || peso > 1000)
+    {
+        tdIMC.textContent = ("Peso incorrecto");
+        pesoEsValido = false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+    if(altura < 0 || altura > 4)
+    {
+        tdIMC.textContent = ("Altura incorrecta");
+        alturaEsValida = false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+    if(pesoEsValido && alturaEsValida)
+    {
+        console.log("Paciente " + nombre);
+        console.log(peso);
+        console.log(altura);
+        var imc = peso / (altura * altura);
+        console.log(imc);
+        tdIMC.textContent = imc.toFixed(1);
+    }
 }
